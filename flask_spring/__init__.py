3f8f0a535ddfcf7e414f7bdfbad6ea5b1f52f089
David Anderson <david@pibcak.net>
2015-09-18 21:06:26 +0100

    Adding in license and setup information

--- flask_spring/__init__.py
+++ flask_spring/__init__.py
@@ -1,20 +1,14 @@
 from springpython.config import YamlConfig, PyContainerConfig
 from springpython.context import ApplicationContext
 
-__author__ = 'david'
-
-# Find the stack on which we want to store the database connection.
-# Starting with Flask 0.9, the _app_ctx_stack is the correct one,
-# before that we need to use the _request_ctx_stack.
-try:
-    from flask import _app_ctx_stack as stack
-except ImportError:
-    from flask import _request_ctx_stack as stack
-
+__author__ = 'David Anderson'
+__version__ = "0.0.1-dev"
 
 class Spring(object):
     def __init__(self, app=None):
         self.app = app
+
+        self._context = None
 
         if app is not None:
             self.init_app(app)
@@ -23,17 +17,10 @@ class Spring(object):
         app.extensions = getattr(app, "extensions", {})
         app.extensions["spring"] = self
 
-        self._context = None
-
         # set any defaults
         app.config.setdefault('SPRING_YAML', 'spring.yml')
         app.config.setdefault('SPRING_XML', None)
         app.config.setdefault('SPRING_OBJS', None)
-
-        if hasattr(app, 'teardown_appcontext'):
-            app.teardown_appcontext(self.teardown)
-        else:
-            app.teardown_request(self.teardown)
 
     @property
     def context(self):
@@ -53,10 +40,3 @@ class Spring(object):
             self._context = ApplicationContext(config_loaders)
 
         return self._context
-
-    def teardown(self, exception):
-        pass
-
-# ctx = stack.top
-#        if hasattr(ctx, 'sqlite3_db'):
-#            ctx.sqlite3_db.close()
